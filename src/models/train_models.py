c307f158011657bb25e83b106c2aa98f0c14d65d
kishore <kishoretirumani249@gmail.com>
2026-07-11 22:16:24 +0530

    11-07-2026

--- src/models/train_models.py
+++ src/models/train_models.py
@@ -1,24 +1,27 @@
+import os
+from sklearn.preprocessing import StandardScaler
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import numpy as np
 import pandas as pd
+import numpy as np
+import pickle
 
 
-class ml_models:
-
-    def __init__(self,data,train_data,test_data):
-        self.data = data
-        self.train_data = train_data
-        self.test_data = test_data
-    def xgbmodel(self):
+def xgbmodel(data,train_data,test_data):
         results = []
+        os.chdir("C:/Users/KISHORE/OneDrive/Desktop/Intelligent Weather Prediction Pipeline/models")
 
-        for target in self.data.columns:
-            X_train = self.train_data.drop(columns=[target])
-            y_train = self.train_data[target]
+        for target in data.loc[:,"Temperature":"is_day"].columns:
 
-            X_test = self.test_data.drop(columns=[target])
-            y_test = self.test_data[target]
+            X_train = train_data.drop(columns=[target])
+            y_train = train_data[target]
+
+            X_test = test_data.drop(columns=[target])
+            y_test = test_data[target]
+
+            scaler = StandardScaler()
+            X_train_scaled = scaler.fit_transform(X_train)
+            X_test_scaled = scaler.transform(X_test)
 
             model = XGBRegressor(
                 n_estimators=300,
@@ -27,18 +30,22 @@ class ml_models:
                 random_state=42
             )
 
-            model.fit(X_train, y_train)
+            model.fit(X_train_scaled,y_train)
 
-            pred = model.predict(X_test)
+            pred = model.predict(X_test_scaled)
 
             mae = mean_absolute_error(y_test, pred)
             rmse = np.sqrt(mean_squared_error(y_test, pred))
             r2 = r2_score(y_test, pred)
 
-            results.append([target, mae, rmse, r2])
+            filename = f"{target}_xgb_model.pkl"
+            results.append([target, mae, rmse, r2, filename])
+            with open(filename,"wb") as file:
+                pickle.dump(model,file)
+            
 
         evaluation = pd.DataFrame(
             results,
-            columns=["Target", "MAE", "RMSE", "R²"]
-            )
-        return evaluation,model
+            columns=["Target", "MAE", "RMSE", "R²", "Filename"]
+        )
+        return evaluation
